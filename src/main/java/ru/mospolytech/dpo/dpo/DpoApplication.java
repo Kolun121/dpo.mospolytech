e8b49440f10ad21994d98c68319362f3c3f938ba
Nikolay <nicola.mosseev@yandex.ru>
2020-09-27 15:49:11 +0300

    Spring создан

---
+++ src/main/java/ru/mospolytech/dpo/dpo/DpoApplication.java
@@ -0,0 +1,13 @@
+package ru.mospolytech.dpo.dpo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DpoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DpoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/mospolytech/dpo/dpo/DpoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mospolytech.dpo.dpo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DpoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
