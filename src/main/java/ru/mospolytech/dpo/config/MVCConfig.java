51d0125c9c93dbbd8874680922179405d164fc57
Nikolay <nicola.mosseev@yandex.ru>
2021-01-26 02:32:08 +0300

    Исправления

--- src/main/java/ru/mospolytech/dpo/config/MVCConfig.java
+++ src/main/java/ru/mospolytech/dpo/config/MVCConfig.java
@@ -1,7 +1,12 @@
 package ru.mospolytech.dpo.config;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,5 +20,17 @@ public class MVCConfig implements WebMvcConfigurer {
 //        registry.addResourceHandler("/img/**")
 //                .addResourceLocations("file:///" + uploadPath + "/");       //WINDOWS
 //                .addResourceLocations("file://" + uploadPath + "/"); //LINUX
+    }
+    
+    @Bean
+    public FilterRegistrationBean encodingFilterRegistrationBean() {
+        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+        registrationBean.setFilter(new CharacterEncodingFilter());
+        Map<String, String> params = new HashMap<>();
+        params.put("encoding", "UTF-8");
+        params.put("forceEncoding", "true");
+        registrationBean.setInitParameters(params);
+        registrationBean.addUrlPatterns("/");
+        return registrationBean;
     }
 }
